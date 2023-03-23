232fdf99a881564e67a8391e6b8d03d3e29c5017
FerPascualF50 <pascualefernando@gmail.com>
2023-03-22 21:33:24 -0300

    l

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import empty from '../assets/empty.png';
 import Container from '@mui/system/Container';
+import CssBaseline from '@mui/material/CssBaseline';
 import '@fontsource/roboto/700.css';
 
 
 const Empty = () => (
-  <Container align='center' >
-    <img src={empty} alt="Rick" />
-    <Container align='center'></Container>
-    <h2>
-
-    You haven't favorites. Go to the Characters page and add ones
-    </h2>
-  </Container>
+  <CssBaseline>
+    <Container align='center' >
+      <img src={empty} alt="Rick" />
+      <Container align='center'></Container>
+      <h2>
+        You haven't favorites. Go to the Characters page and add ones
+      </h2>
+    </Container>
+  </CssBaseline>
 
 );
 
